252dfb548ad47fcaaf6ea71b646aa563e9f7f108
Yahor <yanushkevich-1998@mail.ru>
2020-03-27 21:56:49 +0300

    release v1

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header';
+import Nav from './components/Nav';
+import Content from './components/Content';
+
+
+const App = () => {
+  return (
+    <div className='app-wrapper'>
+      <Header />
+      <Nav />
+      <Content />
+    </div>
+  );
+}
+
+
+
+export default App;
---
+++ src/components/Content.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Content = () => {
+    return  <div className="content">
+    <div>
+      <img src="https://i1.wp.com/static.web-backgrounds.net/uploads/2012/08/City_Landscape_Background.jpg"></img>
+    </div>
+
+    <div>
+      ava+description
+    </div>
+
+    <div>
+      My posts
+        <div>
+        New posts
+        </div>
+      <div>
+        <div>
+          Post 1
+        </div>
+        <div>
+          Post 2
+        </div>
+        
+      </div>
+    </div>
+
+  </div>
+
+}
+
+export default Content;
